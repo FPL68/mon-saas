322fe0acf740f8d7315632fb0573b46b5286b228
Jules <julesdelot@gmail.com>
2026-04-13 19:24:40 +0200

    fix input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,6 +48,7 @@ function App() {
         type="password"
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
+        />
       <button onClick={register}>
         Créer un compte
       </button>
